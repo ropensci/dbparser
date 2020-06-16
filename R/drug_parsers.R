4dcf10c57e5ddc679287fc53d607db033c44a7da
Mohammed Ali <mali@uplandsoftware.com>
2020-06-16 17:47:38 +0200

    fix #98 cmd notes

--- R/drug_parsers.R
+++ R/drug_parsers.R
@@ -2251,6 +2251,65 @@ drug_pharmacology_rec <- function(drug) {
   )
 }
 
+
+#' Extracts the drug_pharmacology elements and return data as tibble.
+#'
+#' \code{drug_pharmacology} returns tibble of drug_pharmacologys main elements.
+#'
+#' This functions extracts the main element of drug_pharmacology node in
+#' drugbank
+#' xml database with the option to save it in a user defined database.
+#' It takes two optional arguments to save the returned tibble in the database
+#' \code{save_table} and \code{database_connection}.
+#' It must be called after \code{\link{read_drugbank_xml_db}} function like
+#' any other parser function.
+#' If \code{\link{read_drugbank_xml_db}} is called before for any reason, so
+#' no need to call it again before calling this function.
+#'
+#' @param save_table boolean, save table in database if true.
+#' @param save_csv boolean, save csv version of parsed tibble if true
+#' @param csv_path location to save csv files into it, default is current
+#' location, save_csv must be true
+#' @param override_csv override existing csv, if any, in case it is true in the
+#'  new parse operation
+#' @param database_connection DBI connection object that holds a connection to
+#' user defined database. If \code{save_table} is enabled without providing
+#' value for this function an error will be thrown.
+#' @return drug_pharmacology attributes tibble
+#' @family drugs
+#' @examples
+#' \dontrun{
+#' # return only the parsed tibble
+#' drug_pharmacology()
+#'
+#' # will throw an error, as database_connection is NULL
+#' drug_pharmacology(save_table = TRUE)
+#'
+#' # save in database in SQLite in memory database and return parsed tibble
+#' sqlite_con <- DBI::dbConnect(RSQLite::SQLite())
+#' drug_pharmacology(save_table = TRUE, database_connection = sqlite_con)
+#'
+#' # save parsed tibble as csv if it does not exist in current
+#' # location and return parsed tibble.
+#' # If the csv exist before read it and return its data.
+#' drug_pharmacology(save_csv = TRUE)
+#'
+#' # save in database, save parsed tibble as csv if it does not exist
+#' # in current location and return parsed tibble.
+#' # If the csv exist before read it and return its data.
+#' drug_pharmacology(save_table = TRUE, save_csv = TRUE,
+#'  database_connection = sqlite_con)
+#'
+#' # save parsed tibble as csv if it does not exist in given location
+#' #  and return parsed tibble.
+#' # If the csv exist before read it and return its data.
+#' drug_pharmacology(save_csv = TRUE, csv_path = TRUE)
+#'
+#' # save parsed tibble as csv if it does not exist in current
+#' # location and return parsed tibble.
+#' # If the csv exist override it and return it.
+#' drug_pharmacology(save_csv = TRUE, csv_path = TRUE, override = TRUE)
+#' }
 #' @export
 drug_pharmacology <-
   function(save_table = FALSE,
@@ -2259,7 +2318,7 @@ drug_pharmacology <-
            override_csv = FALSE,
            database_connection = NULL) {
     check_parameters_validation(save_table, database_connection)
-    path <- get_dataset_full_path("drugs", csv_path)
+    path <- get_dataset_full_path("drugs_pharmacology", csv_path)
     if (!override_csv & file.exists(path)) {
       drugs_pharmacology <- readr::read_csv(path)
     } else {
@@ -2296,7 +2355,7 @@ drug_pharmacology <-
             drugs_pharmacology$metabolism
           ), na.rm = TRUE) + 10, ")"),
           clearance = paste0("varchar(", max(nchar(
-            drugs$clearance
+            drugs_pharmacology$clearance
           ), na.rm = TRUE) + 10, ")"),
           half_life = paste0("varchar(", max(nchar(
             drugs_pharmacology$half_life
