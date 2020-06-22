e2b07f7750676524beb9673757aa29ed1836a8d7
Mohammed Ali <mali@uplandsoftware.com>
2020-06-22 08:41:12 +0200

    #103, refactor cett parsers

--- R/drug_organizm_sub_node_parser.R
+++ R/drug_organizm_sub_node_parser.R
@@ -1,13 +1,1 @@
-# Extract drug carriers df
-get_organizm_rec <- function(r, drug_key) {
-  tibble(
-    id = xmlValue(r[["id"]]),
-    name = xmlValue(r[["name"]]),
-    organism = xmlValue(r[["organism"]]),
-    known_action = xmlValue(r[["known-action"]]),
-    position = ifelse(is.null(xmlGetAttr(r, name = "position")), NA,
-      xmlGetAttr(r, name = "position")
-    ),
-    parent_key = drug_key
-  )
-}
+
--- R/imports.R
+++ R/imports.R
@@ -5,7 +5,7 @@
 #' @importFrom purrr '%>%' map_df map_chr is_empty is_null
 #' @importFrom readr read_csv write_csv
 #' @importFrom RMariaDB MariaDB
-#' @importFrom tibble tibble as_tibble as_tibble_row
+#' @importFrom tibble tibble as_tibble as_tibble_row tibble_row
 #' @importFrom utils unzip
 #' @importFrom XML xmlChildren xmlGetAttr xmlValue xmlToList xmlToDataFrame
 #'  xmlParse xmlRoot xmlSize xmlApply xmlSApply
