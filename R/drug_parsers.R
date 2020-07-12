aba4ba1bcfd30e9bf6156cefd18434b523d58b81
Mohammed Ali <mali@uplandsoftware.com>
2020-07-12 07:29:31 +0200

    #110, refactor drugs syn

--- R/drug_parsers.R
+++ R/drug_parsers.R
@@ -18,7 +18,7 @@ DrugElementsParser <- R6::R6Class(
 
 #' Drug Groups parser
 #'
-#' groups that this drug belongs to. May include any of: approved, vet_approved,
+#' Groups that this drug belongs to. May include any of: approved, vet_approved,
 #'  nutraceutical, illicit, withdrawn, investigational, and experimental.
 #'
 #' @inheritSection drug_all read_drugbank_xml_db
@@ -48,93 +48,6 @@ drug_groups <-
       "drug_groups",
       main_node = "groups"
     )$parse()
-  }
-
-#' Extracts the drug syn element and return data as tibble.
-#'
-#' \code{drug_syn} returns tibble of drug syn elements.
-#'
-#' This functions extracts the syn element of drug node in drugbank
-#' xml database with the option to save it in a predefined database via
-#' passed database connection. It takes two optional arguments to
-#' save the returned tibble in the database \code{save_table} and
-#' \code{database_connection}.
-#' It must be called after \code{\link{read_drugbank_xml_db}} function like
-#' any other parser function.
-#' If \code{\link{read_drugbank_xml_db}} is called before for any reason, so
-#' no need to call it again before calling this function.
-#'
-#' @param save_table boolean, save table in database if true.
-#' @param save_csv boolean, save csv version of parsed tibble if true
-#' @param csv_path location to save csv files into it, default is current
-#' location, save_csv must be true
-#' @param override_csv override existing csv, if any, in case it is true in the
-#'  new parse operation
-#' @param database_connection DBI connection object that holds a connection to
-#' user defined database. If \code{save_table} is enabled without providing
-#' value for this function an error will be thrown.
-#' @return drug syn node attributes tibble
-#' @family drugs
-#' @examples
-#' \dontrun{
-#' # return only the parsed tibble
-#' drug_syn()
-#'
-#' # will throw an error, as database_connection is NULL
-#' drug_syn(save_table = TRUE)
-#'
-#' # save in database in SQLite in memory database and return parsed tibble
-#' sqlite_con <- DBI::dbConnect(RSQLite::SQLite())
-#' drug_syn(save_table = TRUE, database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in current
-#' # location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_syn(save_csv = TRUE)
-#'
-#' # save in database, save parsed tibble as csv if it does not exist
-#' # in current location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_syn(save_table = TRUE, save_csv = TRUE,
-#' database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in given location
-#' # and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_syn(save_csv = TRUE, csv_path = TRUE)
-#'
-#' # save parsed tibble as csv if it does not exist in current
-#' # location and return parsed tibble.
-#' # If the csv exist override it and return it.
-#' drug_syn(save_csv = TRUE, csv_path = TRUE, override = TRUE)
-#' }
-#' @export
-drug_syn <-
-  function(save_table = FALSE,
-           save_csv = FALSE,
-           csv_path = ".",
-           override_csv = FALSE,
-           database_connection = NULL) {
-    check_parameters_validation(save_table, database_connection)
-    path <- get_dataset_full_path("drug_syn", csv_path)
-    if (!override_csv & file.exists(path)) {
-      drug_syn <- readr::read_csv(path)
-    } else {
-      drug_syn <-
-        map_df(pkg_env$children, ~ get_syn_df(.x)) %>% unique()
-      write_csv(drug_syn, save_csv, csv_path)
-    }
-
-
-    if (save_table) {
-      save_drug_sub(
-        con = database_connection,
-        df = drug_syn,
-        table_name = "drug_syn",
-        field_types = list(synonym = "varchar(534)")
-      )
-    }
-    return(drug_syn %>% as_tibble())
   }
 
 #' Extracts the drug products element and return data as tibble.
--- R/drug_synonym_node_parser.R
+++ R/drug_synonym_node_parser.R
@@ -1,16 +1,73 @@
-get_syn_df <- function(rec) {
-  if (xmlSize(rec[["synonyms"]]) < 1) return()
-  syn <- as_tibble(t(xmlSApply(rec[["synonyms"]], syn_rec)))
-  syn[["drugbank-id"]] <- xmlValue(rec[["drugbank-id"]])
-  return(syn)
+DrugSynonymsParser <- R6::R6Class(
+  "DrugSynonymsParser",
+  inherit = AbstractParser,
+  private = list(
+    parse_record = function() {
+      drugs <-  xmlChildren(pkg_env$root)
+      pb <- progress_bar$new(total = xmlSize(drugs))
+      map_df(drugs, ~ private$get_syn_df(.x, pb)) %>% unique()
+    },
+    get_syn_df = function(rec, pb) {
+      pb$tick()
+      if (xmlSize(rec[["synonyms"]]) < 1) return()
+      syn <- as_tibble(t(xmlSApply(rec[["synonyms"]], private$syn_rec)))
+      syn[["drugbank-id"]] <- xmlValue(rec[["drugbank-id"]])
+      return(syn)
 
-}
-
-syn_rec <- function(rec) {
-  c(
-    synonym = xmlValue(rec),
-    language = xmlGetAttr(rec, name = "language"),
-    coder = xmlGetAttr(rec,
-                       name = "coder")
+    },
+    syn_rec = function(rec) {
+      c(
+        synonym = xmlValue(rec),
+        language = xmlGetAttr(rec, name = "language"),
+        coder = xmlGetAttr(rec,
+                           name = "coder")
+      )
+    },
+    save_db_table = function(parsed_tbl) {
+      if (private$save_table) {
+        save_drug_sub(
+          con = private$database_connection,
+          df = parsed_tbl,
+          table_name = "drug_syn",
+          field_types = list(synonym = "varchar(534)")
+        )
+      }
+    }
   )
-}
+)
+
+#' Drug Synonyms parser
+#'
+#' Other names or identifiers that are associated with this drug.
+#'
+#' @inheritSection drug_all read_drugbank_xml_db
+#' @inheritParams drug_all
+#'
+#' @return  a tibble with 3 variables:
+#' \describe{
+#'  \item{language}{Names of the drug in languages other than English.}
+#'  \item{coder}{Organisation or source providing the synonym. For example,
+#'   INN indicates the synonym is an International Nonproprietary Name,
+#'   while IUPAC indicates the synonym is the nomenclature designated by the
+#'   International Union of Pure and Applied Chemistry.}
+#'  \item{\emph{drugbank_id}}{drugbank id}
+#' }
+#' @family drugs
+#'
+#' @inherit drug_all examples
+#' @export
+drug_syn <-
+  function(save_table = FALSE,
+           save_csv = FALSE,
+           csv_path = ".",
+           override_csv = FALSE,
+           database_connection = NULL) {
+    DrugSynonymsParser$new(
+      save_table,
+      save_csv,
+      csv_path,
+      override_csv,
+      database_connection,
+      "drug_syn",
+    )$parse()
+  }
