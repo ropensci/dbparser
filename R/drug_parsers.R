9a3daaa1b78bf2724bb1dfffece00f7d9512a159
Mohammed Ali <mali@uplandsoftware.com>
2020-07-12 15:41:28 +0200

    #110, refactor snp-effects

--- R/drug_parsers.R
+++ R/drug_parsers.R
@@ -768,64 +768,33 @@ drug_external_links <-
     )$parse()
   }
 
-#' Extracts the drug snp effects element and return data as tibble.
+#' Drug SNP Effects parser
 #'
-#' \code{drug_snp_effects} returns tibble of snp effects groups elements.
+#' A list of single nucleotide polymorphisms (SNPs) relevent to drug activity or
+#'  metabolism, and the effects these may have on pharmacological activity.
+#'  SNP effects in the patient may require close monitoring, an increase or
+#'  decrease in dose, or a change in therapy.
 #'
-#' This functions extracts the snp effects element of drug node in drugbank
-#' xml database with the option to save it in a predefined database via
-#' passed database connection. It takes two optional arguments to
-#' save the returned tibble in the database \code{save_table} and
-#' \code{database_connection}.
-#' It must be called after \code{\link{read_drugbank_xml_db}} function like
-#' any other parser function.
-#' If \code{\link{read_drugbank_xml_db}} is called before for any reason, so
-#' no need to call it again before calling this function.
+#' @inheritSection drug_all read_drugbank_xml_db
+#' @inheritParams drug_all
 #'
-#' @param save_table boolean, save table in database if true.
-#' @param save_csv boolean, save csv version of parsed tibble if true
-#' @param csv_path location to save csv files into it, default is current
-#' location, save_csv must be true
-#' @param override_csv override existing csv, if any, in case it is true in the
-#'  new parse operation
-#' @param database_connection DBI connection object that holds a connection to
-#' user defined database. If \code{save_table} is enabled without providing
-#' value for this function an error will be thrown.
-#' @return drug snp effects node attributes tibble
-#' @family drugs
-#' @examples
-#' \dontrun{
-#' # return only the parsed tibble
-#' drug_snp_effects()
-#'
-#' # will throw an error, as database_connection is NULL
-#' drug_snp_effects(save_table = TRUE)
-#'
-#' # save in database in SQLite in memory database and return parsed tibble
-#' sqlite_con <- DBI::dbConnect(RSQLite::SQLite())
-#' drug_snp_effects(save_table = TRUE, database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in current location
-#' # and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_snp_effects(save_csv = TRUE)
-#'
-#' # save in database, save parsed tibble as csv if it does not exist
-#' # in current location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_snp_effects(save_table = TRUE, save_csv = TRUE,
-#'  database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in given location
-#' # and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_snp_effects(save_csv = TRUE, csv_path = TRUE)
-#'
-#' # save parsed tibble as csv if it does not exist in current location
-#' # and return parsed tibble.
-#' # If the csv exist override it and return it.
-#' drug_snp_effects(save_csv = TRUE, csv_path = TRUE, override = TRUE)
+#' @return  a tibble with the following variables:
+#' \describe{
+#'  \item{protein-name}{Proteins involved in this SNP.}
+#'  \item{gene-symbol}{Genes involved in this SNP.}
+#'  \item{uniprot-id}{Universal Protein Resource (UniProt) identifiers for
+#'  proteins involved in this pathway.}
+#'  \item{rs-id}{	The SNP Database identifier for this single nucleotide
+#'  polymorphism.}
+#'  \item{allele}{The alleles associated with the identified SNP.}
+#'  \item{defining-change}{}
+#'  \item{description}{A written description of the SNP effects.}
+#'  \item{pubmed-id	}{Reference to PubMed article.}
+#'  \item{\emph{drugbank_id}}{drugbank id}
 #' }
+#' @family drugs
+#'
+#' @inherit drug_all examples
 #' @export
 drug_snp_effects <-
   function(save_table = FALSE,
@@ -833,24 +802,15 @@ drug_snp_effects <-
            csv_path = ".",
            override_csv = FALSE,
            database_connection = NULL) {
-    check_parameters_validation(save_table, database_connection)
-    path <- get_dataset_full_path("drug_snp_effects", csv_path)
-    if (!override_csv & file.exists(path)) {
-      drug_snp_effects <- readr::read_csv(path)
-    } else {
-      drug_snp_effects <-
-        map_df(pkg_env$children, ~ drug_sub_df(.x, "snp-effects")) %>% unique()
-
-      write_csv(drug_snp_effects, save_csv, csv_path)
-    }
-
-
-    if (save_table) {
-      save_drug_sub(con = database_connection,
-                    df = drug_snp_effects,
-                    table_name = "drug_snp_effects")
-    }
-    return(drug_snp_effects %>% as_tibble())
+    DrugElementsParser$new(
+      save_table,
+      save_csv,
+      csv_path,
+      override_csv,
+      database_connection,
+      "drug_snp_effects",
+      main_node = "snp-effects"
+    )$parse()
   }
 
 #' Extracts the drug snp adverse drug reactions element and return data as
