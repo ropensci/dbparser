19519c23c35067af5998f83f97aee8f1551a1070
Mohammed Ali <mali@uplandsoftware.com>
2020-07-12 16:56:11 +0200

    #110, refactor manufacturersParser

--- R/drug_manufacturer_node_parser.R
+++ R/drug_manufacturer_node_parser.R
@@ -1,100 +1,63 @@
-get_manufacturer_df <- function(rec) {
-  if (xmlSize(rec[["manufacturers"]]) < 1)
-    return()
-  manufacturers <- as_tibble(t(xmlSApply(rec[["manufacturers"]],
-                                         manufacturer_rec)))
-  manufacturers[["drugbank-id"]] <- xmlValue(rec[["drugbank-id"]])
-  return(manufacturers)
-
-}
-
-manufacturer_rec <- function(rec) {
-  c(
-    manufacturer = xmlValue(rec),
-    generic = xmlGetAttr(node = rec, name = "generic"),
-    url = xmlGetAttr(node = rec, name = "url")
+ManufacturersParser <- R6::R6Class(
+  "ManufacturersParser",
+  inherit = AbstractParser,
+  private = list(
+    parse_record = function() {
+      drugs <-  xmlChildren(pkg_env$root)
+      pb <- progress_bar$new(total = xmlSize(drugs))
+      return(map_df(drugs, ~ private$get_manufacturer_df(.x, pb)) %>%
+               unique())
+    },
+    get_manufacturer_df = function(rec, pb) {
+      pb$tick()
+      if (xmlSize(rec[["manufacturers"]]) < 1)
+        return()
+      manufacturers <- as_tibble(t(xmlSApply(rec[["manufacturers"]],
+                                             private$manufacturer_rec)))
+      manufacturers[["drugbank-id"]] <- xmlValue(rec[["drugbank-id"]])
+      return(manufacturers)
+    },
+    manufacturer_rec = function(rec) {
+      c(
+        manufacturer = xmlValue(rec),
+        generic = xmlGetAttr(node = rec, name = "generic"),
+        url = xmlGetAttr(node = rec, name = "url")
+      )
+    }
   )
-}
-#' Extracts the drug manufacturers element and return data as tibble.
+)
+
+#' Drug Manufacturers parser
 #'
-#' \code{drug_manufacturers} returns tibble of drug manufacturers
-#' elements.
+#' A list of companies that are manufacturing the commercially available forms
+#' of this drug that are available in Canada and the Unites States.
 #'
-#' This functions extracts the manufacturers element of drug node in
-#'  \strong{DrugBank}
-#' xml database with the option to save it in a predefined database via
-#' passed database connection. It takes two optional arguments to
-#' save the returned tibble in the database \code{save_table} and
-#'  \code{database_connection}.
-#' It must be called after \code{\link{read_drugbank_xml_db}} function like
-#' any other parser function.
-#' If \code{\link{read_drugbank_xml_db}} is called before for any reason, so
-#' no need to call it again before calling this function.
+#' @inheritSection drug_all read_drugbank_xml_db
+#' @inheritParams drug_all
 #'
-#' @param save_table boolean, save table in database if true.
-#' @param save_csv boolean, save csv version of parsed tibble if true
-#' @param csv_path location to save csv files into it, default is current
-#' location, save_csv must be true
-#' @param override_csv override existing csv, if any, in case it is true in the
-#'  new parse operation
-#' @param database_connection DBI connection object that holds a connection to
-#' user defined database. If \code{save_table} is enabled without providing
-#' value for this function an error will be thrown.
-#'
-#' @return drug manufacturers node attributes date frame
-#' @family drugs
-#' @examples
-#' \dontrun{
-#' # return only the parsed tibble
-#' drug_manufacturers()
-#'
-#' # will throw an error, as database_connection is NULL
-#' drug_manufacturers(save_table = TRUE)
-#'
-#' # save parsed tibble as csv if it does not exist in current
-#' # location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_manufacturers(save_csv = TRUE)
-#'
-#' # save in database, save parsed tibble as csv if it does not exist
-#' # in current location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_manufacturers(save_table = TRUE, save_csv = TRUE,
-#' database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in given location
-#' # and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_manufacturers(save_csv = TRUE, csv_path = TRUE)
-#'
-#' # save parsed tibble as csv if it does not exist in current
-#' # location and return parsed tibble.
-#' # If the csv exist override it and return it.
-#' drug_manufacturers(save_csv = TRUE, csv_path = TRUE, override = TRUE)
+#' @return  a tibble with the following variables:
+#' \describe{
+#'  \item{generic}{A list of companies that are manufacturing the generic
+#'   form of the drug.}
+#'  \item{url}{A link to the companies that are manufacturing the drug.}
+#'  \item{\emph{drugbank_id}}{drugbank id}
 #' }
+#' @family drugs
+#'
+#' @inherit drug_all examples
 #' @export
+
 drug_manufacturers <- function(save_table = FALSE,
                                save_csv = FALSE,
                                csv_path = ".",
                                override_csv = FALSE,
                                database_connection = NULL) {
-  check_parameters_validation(save_table, database_connection)
-  path <-
-    get_dataset_full_path("drug_manufacturers", csv_path)
-  if (!override_csv & file.exists(path)) {
-    drug_manufacturers <- readr::read_csv(path)
-  } else {
-    drug_manufacturers <-
-      map_df(pkg_env$children, ~ get_manufacturer_df(.x)) %>%
-      unique()
-    write_csv(drug_manufacturers, save_csv, csv_path)
-  }
-
-
-  if (save_table) {
-    save_drug_sub(con = database_connection,
-                  df = drug_manufacturers,
-                  table_name = "drug_manufacturers")
-  }
-  return(drug_manufacturers %>% as_tibble())
+  ManufacturersParser$new(
+    save_table,
+    save_csv,
+    csv_path,
+    override_csv,
+    database_connection,
+    "drug_manufacturers"
+  )$parse()
 }
