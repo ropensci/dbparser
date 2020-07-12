20dfd5ced02875518bc8c7e443c6fb4734fa64c3
Mohammed Ali <mali@uplandsoftware.com>
2020-07-12 13:33:46 +0200

    #110, refactor calculated properties

--- R/drug_parsers.R
+++ R/drug_parsers.R
@@ -135,95 +135,42 @@ drug_products <-
     )$parse()
   }
 
-#' Extracts the drug calculated properties element and return data as tibble.
+#' Drug Calculated Properties parser
 #'
-#' \code{drug_calc_prop} returns tibble of drug calculated
-#' properties elements.
+#' Drug properties that have been predicted by ChemAxon or ALOGPS based on the
+#' inputed chemical structure. Associated links below will redirect to
+#' descriptions of the specific term.
 #'
-#' This functions extracts the calculated properties element of drug node in
-#'  drugbank
-#' xml database with the option to save it in a predefined database via
-#' passed database connection. It takes two optional arguments to
-#' save the returned tibble in the database \code{save_table} and
-#' \code{database_connection}.
-#' It must be called after \code{\link{read_drugbank_xml_db}} function like
-#' any other parser function.
-#' If \code{\link{read_drugbank_xml_db}} is called before for any reason, so
-#' no need to call it again before calling this function.
+#' @inheritSection drug_all read_drugbank_xml_db
+#' @inheritParams drug_all
 #'
-#' @param save_table boolean, save table in database if true.
-#' @param save_csv boolean, save csv version of parsed tibble if true
-#' @param csv_path location to save csv files into it, default is current
-#' location, save_csv must be true
-#' @param override_csv override existing csv, if any, in case it is true in the
-#'  new parse operation
-#' @param database_connection DBI connection object that holds a connection to
-#' user defined database. If \code{save_table} is enabled without providing
-#' value for this function an error will be thrown.
-#' @return drug calculated properties node attributes tibble
-#' @family drugs
-#' @examples
-#' \dontrun{
-#' # return only the parsed tibble
-#' drug_calc_prop()
-#'
-#' # will throw an error, as database_connection is NULL
-#' drug_calc_prop(save_table = TRUE)
-#'
-#' # save in database in SQLite in memory database and return parsed tibble
-#' sqlite_con <- DBI::dbConnect(RSQLite::SQLite())
-#' drug_calc_prop(save_table = TRUE, database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in current
-#' # location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_calc_prop(save_csv = TRUE)
-#'
-#' # save in database, save parsed tibble as csv if it does not exist
-#' # in current location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_calc_prop(save_table = TRUE, save_csv = TRUE,
-#'  database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in given location
-#' # and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_calc_prop(save_csv = TRUE, csv_path = TRUE)
-#'
-#' # save parsed tibble as csv if it does not exist in current
-#' # location and return parsed tibble.
-#' # If the csv exist override it and return it.
-#' drug_calc_prop(
-#'   save_csv = TRUE, csv_path = TRUE,
-#'   override = TRUE
-#' )
+#' @return  a tibble with 4 variables:
+#' \describe{
+#'  \item{kind}{Name of the property.}
+#'  \item{value}{Predicted physicochemical properties; obtained by the use of
+#'  prediction software such as ALGOPS and ChemAxon.}
+#'  \item{source}{Name of the software used to calculate this property,
+#'  either ChemAxon or ALOGPS.}
+#'  \item{\emph{drugbank_id}}{drugbank id}
 #' }
+#' @family drugs
+#'
+#' @inherit drug_all examples
 #' @export
 drug_calc_prop <- function(save_table = FALSE,
                            save_csv = FALSE,
                            csv_path = ".",
                            override_csv = FALSE,
                            database_connection = NULL) {
-  check_parameters_validation(save_table, database_connection)
-  path <-
-    get_dataset_full_path("drug_calculated_properties", csv_path)
-  if (!override_csv & file.exists(path)) {
-    drug_calculated_properties <- readr::read_csv(path)
-  } else {
-    drug_calculated_properties <-
-      map_df(pkg_env$children,
-             ~ drug_sub_df(.x, "calculated-properties")) %>% unique()
-
-    write_csv(drug_calculated_properties, save_csv, csv_path)
-  }
-
-
-  if (save_table) {
-    save_drug_sub(con = database_connection,
-                  df = drug_calculated_properties,
-                  table_name = "drug_calculated_properties")
-  }
-  return(drug_calculated_properties %>% as_tibble())
+  DrugElementsParser$new(
+    save_table,
+    save_csv,
+    csv_path,
+    override_csv,
+    database_connection,
+    "drug_calculated_properties",
+    main_node = "calculated-properties"
+  )$parse()
 }
 
 #' Extracts the drug international brands and return data as tibble.
