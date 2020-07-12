0e2efae975dfa59995422d82f5a4cf5a1202c793
Mohammed Ali <mali@uplandsoftware.com>
2020-07-12 14:45:51 +0200

    #110, refactor pdb-entries

--- R/drug_parsers.R
+++ R/drug_parsers.R
@@ -16,7 +16,8 @@ DrugElementsParser <- R6::R6Class(
             c("brand", "company","drugbank-id"),
           "affected-organisms" = names(parsed_tbl) <-
             c("affected_organism", "drugbank_id"),
-          "ahfs-codes" = names(parsed_tbl) <- c("ahfs_code", "drugbank_id")
+          "ahfs-codes" = names(parsed_tbl) <- c("ahfs_code", "drugbank_id"),
+          "pdb-entries" = names(parsed_tbl) <- c("pdb_entry", "drugbank_id")
         )
       }
       return(parsed_tbl)
@@ -484,64 +485,21 @@ drug_ahfs_codes <-
     )$parse()
   }
 
-#' Extracts the drug pdb entries element and return data as tibble.
+#' Drug pdb-entries parser
 #'
-#' \code{drug_pdb_entries} returns tibble of drug pdb entries elements.
+#' Protein Data Bank (PDB) identifiers for this drug.
 #'
-#' This functions extracts the pdb entries element of drug node in drugbank
-#' xml database with the option to save it in a predefined database via
-#' passed database connection. It takes two optional arguments to
-#' save the returned tibble in the database \code{save_table} and
-#'  \code{database_connection}.
-#' It must be called after \code{\link{read_drugbank_xml_db}} function like
-#' any other parser function.
-#' If \code{\link{read_drugbank_xml_db}} is called before for any reason, so
-#' no need to call it again before calling this function.
+#' @inheritSection drug_all read_drugbank_xml_db
+#' @inheritParams drug_all
 #'
-#' @param save_table boolean, save table in database if true.
-#' @param save_csv boolean, save csv version of parsed tibble if true
-#' @param csv_path location to save csv files into it, default is current
-#' location, save_csv must be true
-#' @param override_csv override existing csv, if any, in case it is true in the
-#'  new parse operation
-#' @param database_connection DBI connection object that holds a connection to
-#' user defined database. If \code{save_table} is enabled without providing
-#' value for this function an error will be thrown.
-#' @return drug pdb entries node attributes tibble
-#' @family drugs
-#' @examples
-#' \dontrun{
-#' # return only the parsed tibble
-#' drug_pdb_entries()
-#'
-#' # will throw an error, as database_connection is NULL
-#' drug_pdb_entries(save_table = TRUE)
-#'
-#' # save in database in SQLite in memory database and return parsed tibble
-#' sqlite_con <- DBI::dbConnect(RSQLite::SQLite())
-#' drug_pdb_entries(save_table = TRUE, database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in current
-#' # location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_pdb_entries(save_csv = TRUE)
-#'
-#' # save in database, save parsed tibble as csv if it does not exist
-#' # in current location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_pdb_entries(save_table = TRUE, save_csv = TRUE,
-#'  database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in given location
-#' # and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_pdb_entries(save_csv = TRUE, csv_path = TRUE)
-#'
-#' # save parsed tibble as csv if it does not exist in current
-#' # location and return parsed tibble.
-#' # If the csv exist override it and return it.
-#' drug_pdb_entries(save_csv = TRUE, csv_path = TRUE, override = TRUE)
+#' @return  a tibble with the following variables:
+#' \describe{
+#'  \item{pdb-entry}{}
+#'  \item{\emph{drugbank_id}}{drugbank id}
 #' }
+#' @family drugs
+#'
+#' @inherit drug_all examples
 #' @export
 drug_pdb_entries <-
   function(save_table = FALSE,
@@ -549,26 +507,15 @@ drug_pdb_entries <-
            csv_path = ".",
            override_csv = FALSE,
            database_connection = NULL) {
-    check_parameters_validation(save_table, database_connection)
-    path <- get_dataset_full_path("drug_pdb_entries", csv_path)
-    if (!override_csv & file.exists(path)) {
-      drug_pdb_entries <- readr::read_csv(path)
-    } else {
-      drug_pdb_entries <-
-        map_df(pkg_env$children, ~ drug_sub_df(.x, "pdb-entries")) %>% unique()
-      write_csv(drug_pdb_entries, save_csv, csv_path)
-    }
-
-    if (nrow(drug_pdb_entries) > 0) {
-      colnames(drug_pdb_entries) <- c("pdb_entry", "drugbank_id")
-    }
-
-    if (save_table) {
-      save_drug_sub(con = database_connection,
-                    df = drug_pdb_entries,
-                    table_name = "drug_pdb_entries")
-    }
-    return(drug_pdb_entries %>% as_tibble())
+    DrugElementsParser$new(
+      save_table,
+      save_csv,
+      csv_path,
+      override_csv,
+      database_connection,
+      "drug_pdb_entries",
+      main_node = "pdb-entries"
+    )$parse()
   }
 
 #' Extracts the drug patents element and return data as tibble.
