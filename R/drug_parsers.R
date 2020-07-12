645950974aec5d92994bcc28cf09348bd2a4f997
Mohammed Ali <mali@uplandsoftware.com>
2020-07-12 14:57:29 +0200

    #110, refactor food-interactions

--- R/drug_parsers.R
+++ R/drug_parsers.R
@@ -17,7 +17,9 @@ DrugElementsParser <- R6::R6Class(
           "affected-organisms" = names(parsed_tbl) <-
             c("affected_organism", "drugbank_id"),
           "ahfs-codes" = names(parsed_tbl) <- c("ahfs_code", "drugbank_id"),
-          "pdb-entries" = names(parsed_tbl) <- c("pdb_entry", "drugbank_id")
+          "pdb-entries" = names(parsed_tbl) <- c("pdb_entry", "drugbank_id"),
+          "food-interactions" = names(parsed_tbl) <-
+            c("food_interaction", "drugbank_id")
         )
       }
       return(parsed_tbl)
@@ -559,68 +561,21 @@ drug_patents <-
     )$parse()
   }
 
-#' Extracts the drug food interactions element and return data as tibble.
+#' Drug Groups parser
 #'
-#' \code{drug_food_interactions} returns tibble of drug food
-#' interactions elements.
+#' Food that may interact with this drug.
 #'
-#' This functions extracts the food interactions element of drug node in
-#' drugbank xml database with the option to save it in a predefined database via
-#' passed database connection. It takes two optional arguments to
-#' save the returned tibble in the database \code{save_table} and
-#' \code{database_connection}.
-#' It must be called after \code{\link{read_drugbank_xml_db}} function like
-#' any other parser function.
-#' If \code{\link{read_drugbank_xml_db}} is called before for any reason, so
-#' no need to call it again before calling this function.
+#' @inheritSection drug_all read_drugbank_xml_db
+#' @inheritParams drug_all
 #'
-#' @param save_table boolean, save table in database if true.
-#' @param save_csv boolean, save csv version of parsed tibble if true
-#' @param csv_path location to save csv files into it, default is current
-#' location, save_csv must be true
-#' @param override_csv override existing csv, if any, in case it is true in the
-#'  new parse operation
-#' @param database_connection DBI connection object that holds a connection to
-#' user defined database. If \code{save_table} is enabled without providing
-#' value for this function an error will be thrown.
-#' @return drug food interactions node attributes tibble
-#' @family drugs
-#' @examples
-#' \dontrun{
-#' # return only the parsed tibble
-#' drug_food_interactions()
-#'
-#' # will throw an error, as database_connection is NULL
-#' drug_food_interactions(save_table = TRUE)
-#'
-#' # save in database in SQLite in memory database and return parsed tibble
-#' sqlite_con <- DBI::dbConnect(RSQLite::SQLite())
-#' drug_food_interactions(save_table = TRUE, database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in current location
-#' # and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_food_interactions(save_csv = TRUE)
-#'
-#' # save in database, save parsed tibble as csv if it does not exist
-#' # in current location and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_food_interactions(save_table = TRUE, save_csv = TRUE,
-#'  database_connection = sqlite_con)
-#'
-#' # save parsed tibble as csv if it does not exist in given location
-#' # and return parsed tibble.
-#' # If the csv exist before read it and return its data.
-#' drug_food_interactions(save_csv = TRUE, csv_path = TRUE)
-#'
-#' # save parsed tibble as csv if it does not exist in current location
-#' #  and return parsed tibble.
-#' # If the csv exist override it and return it.
-#' drug_food_interactions(
-#'   save_csv = TRUE, csv_path = TRUE,
-#'   override = TRUE
-#' )
+#' @return  a tibble with the following variables:
+#' \describe{
+#'  \item{food-interaction}{}
+#'  \item{\emph{drugbank_id}}{drugbank id}
 #' }
+#' @family drugs
+#'
+#' @inherit drug_all examples
 #' @export
 drug_food_interactions <-
   function(save_table = FALSE,
@@ -628,31 +583,15 @@ drug_food_interactions <-
            csv_path = ".",
            override_csv = FALSE,
            database_connection = NULL) {
-    check_parameters_validation(save_table, database_connection)
-    path <-
-      get_dataset_full_path("drug_food_interactions", csv_path)
-    if (!override_csv & file.exists(path)) {
-      drug_food_interactions <- readr::read_csv(path)
-    } else {
-      drug_food_interactions <-
-        map_df(pkg_env$children, ~ drug_sub_df(.x, "food-interactions")) %>%
-        unique()
-      write_csv(drug_food_interactions, save_csv, csv_path)
-    }
-
-
-
-    if (nrow(drug_food_interactions) > 0) {
-      colnames(drug_food_interactions) <-
-        c("food_interaction", "drugbank_id")
-    }
-
-    if (save_table) {
-      save_drug_sub(con = database_connection,
-                    df = drug_food_interactions,
-                    table_name = "drug_food_interactions")
-    }
-    return(drug_food_interactions %>% as_tibble())
+    DrugElementsParser$new(
+      save_table,
+      save_csv,
+      csv_path,
+      override_csv,
+      database_connection,
+      "drug_food_interactions",
+      main_node = "food-interactions"
+    )$parse()
   }
 
 #' Extracts the drug interactions element and return data as tibble.
