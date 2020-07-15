4a88be45b8df5ed352c7dcb0d56344b3133bbbdb
Mohammed Ali <mali@uplandsoftware.com>
2020-07-15 04:15:49 +0200

    #110, refactor prices

--- R/drug_price_node_parser.R
+++ R/drug_price_node_parser.R
@@ -1,24 +1,36 @@
-# Extract drug prices df
-get_price_rec <- function(r, drug_key) {
-  tibble_row(
-    description = xmlValue(r[["description"]]),
-    currency = xmlGetAttr(r[["cost"]], name = "currency"),
-    cost = xmlValue(r[["cost"]]),
-    unit = xmlValue(r[["unit"]]),
-    parent_key = drug_key
+PricesParser <- R6::R6Class(
+  "PricesParser",
+  inherit = AbstractParser,
+  private = list(
+    parse_record = function() {
+      drugs <-  xmlChildren(pkg_env$root)
+      pb <- progress_bar$new(total = xmlSize(drugs))
+      map_df(drugs, ~ private$get_prices_df(., pb)) %>%
+        unique()
+    },
+    get_prices_df = function(rec, pb) {
+      pb$tick()
+      return(map_df(
+        xmlChildren(rec[["prices"]]),
+        ~ private$get_price_rec(., xmlValue(rec[["drugbank-id"]]))
+      ))
+    },
+    get_price_rec = function(r, drug_key) {
+      tibble_row(
+        description = xmlValue(r[["description"]]),
+        currency = xmlGetAttr(r[["cost"]], name = "currency"),
+        cost = xmlValue(r[["cost"]]),
+        unit = xmlValue(r[["unit"]]),
+        parent_key = drug_key
+      )
+    }
   )
-}
+)
 
-get_prices_df <- function(rec) {
-  return(map_df(
-    xmlChildren(rec[["prices"]]),
-    ~ get_price_rec(., xmlValue(rec[["drugbank-id"]]))
-  ))
-}
 
 #' Drug Prices Parsers
 #'
-#' Collection of related actions
+#' Unit drug prices
 #'
 #' @inheritSection drug_all read_drugbank_xml_db
 #' @inheritParams drug_all
@@ -26,8 +38,8 @@ get_prices_df <- function(rec) {
 #' @return a tibble with 5 variables:
 #' \describe{
 #'   \item{description}{}
-#'   \item{cost}{}
-#'   \item{currency}{}
+#'   \item{cost}{Drug price per unit}
+#'   \item{currency}{Currency of price, example: US.}
 #'   \item{unit}{}
 #'   \item{parent_id}{drugbank id}
 #' }
@@ -37,25 +49,12 @@ get_prices_df <- function(rec) {
 drug_prices <- function(save_table = FALSE, save_csv = FALSE,
                               csv_path = ".", override_csv = FALSE,
                         database_connection = NULL) {
-  check_parameters_validation(save_table, database_connection)
-  path <-
-    get_dataset_full_path("drug_prices", csv_path)
-  if (!override_csv & file.exists(path)) {
-    drug_prices <- readr::read_csv(path)
-  } else {
-    drug_prices <-
-      map_df(pkg_env$children, ~ get_prices_df(.x)) %>%
-      unique()
-
-    write_csv(drug_prices, save_csv, csv_path)
-  }
-
-  if (save_table) {
-    save_drug_sub(
-      con = database_connection,
-      df = drug_prices,
-      table_name = "drug_prices"
-    )
-  }
-  return(drug_prices)
+  PricesParser$new(
+    save_table,
+    save_csv,
+    csv_path,
+    override_csv,
+    database_connection,
+    "drug_prices"
+  )$parse()
 }
