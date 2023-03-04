383f0ec65d7325d46b3b151301bd7e5c5c92619f
Mohammed Ali <mohammed@aggregate-genius.com>
2023-03-04 20:20:12 +0200

    #141, imported stack function

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,7 @@
 #' @importFrom progress progress_bar
 #' @importFrom purrr '%>%' map_df map_chr is_empty is_null
 #' @importFrom tibble tibble as_tibble as_tibble_row tibble_row
-#' @importFrom utils unzip
+#' @importFrom utils unzip stack
 #' @importFrom XML xmlChildren xmlGetAttr xmlValue xmlToList xmlToDataFrame
 #'  xmlParse xmlRoot xmlSize xmlApply xmlSApply
 NULL
