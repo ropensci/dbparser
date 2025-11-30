8b1136fd4b2918c10894cfcb949f7172f982ab6e
Mohammed Ali <mohammed@aggregate-genius.com>
2025-11-30 08:08:57 +0200

    #177, Added subset dvobject metadata unit tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,18 +1,19 @@
 context("test utils functions")
 
 library(testthat)
+local_edition(3)
 
-biotech <- "drugbank_record_biotech.xml"
 test_that(
   desc = "Read dvobject metadata",
   code = {
     dvobj <- readRDS(system.file("two_drugs.RDS",
                                  package = "dbparser"))
 
-    new_dvobj <- testthat::expect_warning(
-      subset_drugbank_dvobject(dvobject = dvobj,
-                               drug_ids = ""),
+     expect_warning(
+       new_dvobj <- subset_drugbank_dvobject(dvobject = dvobj,
+                                             drug_ids = ""),
       "`drug_ids` is empty. Returning NULL")
+
     expect_equal(length(new_dvobj), 0)
 
     new_dvobj <- subset_drugbank_dvobject(dvobject = dvobj,
@@ -20,5 +21,6 @@ test_that(
     expect_equal(length(new_dvobj), 5)
     expect_equal(NROW(new_dvobj$drugs$general_information), 1)
     expect_equal(length(new_dvobj$cett), 1)
+    expect_snapshot(show_dvobject_metadata(new_dvobj))
   }
 )
